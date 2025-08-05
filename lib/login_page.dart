0f5130e89f0748025fd62c48878a064532320429
Arjaaa <dhitoarza@gmail.com>
2025-08-05 09:31:52 +0700

    update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:latihan1/kalkulator_page.dart';
 import 'package:latihan1/register_page.dart';
+import 'package:latihan1/widget/widgetbutton.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,18 +14,63 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  String statusLogin = "";
+  void _login() {
+    String email = "a@gmail.com";
+    String password = "5";
+
+    String inputEmail = _emailController.text;
+    String inputPassword = _passwordController.text;
+
+    if (inputEmail.isEmpty || inputPassword.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Email dan password tidak boleh kosong!"),
+          backgroundColor: Colors.red,
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else if (inputEmail == email && inputPassword == password) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Login berhasil dengan email: $inputEmail"),
+          backgroundColor: Colors.green,
+          duration: const Duration(seconds: 2),
+        ),
+      );
+      Future.delayed(const Duration(seconds: 1), () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const KalkulatorPage()),
+        );
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Login gagal! Email atau password salah."),
+          backgroundColor: Colors.red,
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  void _register() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const Register()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login Page")),
+      appBar: AppBar(title: const Text("Login Page")),
       body: Container(
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               "Welcome to our first App",
               style: TextStyle(
                 fontSize: 20,
@@ -32,99 +78,47 @@ class _LoginPageState extends State<LoginPage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Text("please login using your email and password"),
+            const Text("please login using your email and password"),
             Center(
               child: Image.asset('images/logo.png', width: 100, height: 100),
             ),
-
             TextField(
               controller: _emailController,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: "Email",
                 border: OutlineInputBorder(),
               ),
             ),
-
-            SizedBox(height: 10),
-
+            const SizedBox(height: 10),
             TextField(
               controller: _passwordController,
               obscureText: true,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: "Password",
                 border: OutlineInputBorder(),
               ),
             ),
-
             Container(
-              margin: EdgeInsets.only(top: 25),
+              margin: const EdgeInsets.only(top: 25),
               child: Center(
                 child: Column(
                   children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        String email = "a@gmail.com";
-                        String password = "5";
-
-                        String inputEmail = _emailController.text;
-                        String inputPassword = _passwordController.text;
-
-                        if (inputEmail.isEmpty || inputPassword.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                "Email dan password tidak boleh kosong!",
-                              ),
-                              backgroundColor: Colors.red,
-                              duration: Duration(seconds: 2),
-                            ),
-                          );
-                        } else if (inputEmail == email &&
-                            inputPassword == password) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                "Login berhasil dengan email: $inputEmail",
-                              ),
-                              backgroundColor: Colors.green,
-                              duration: const Duration(seconds: 2),
-                            ),
-                          );
-                          Future.delayed(const Duration(seconds: 1), () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const KalkulatorPage(),
-                              ),
-                            );
-                          });
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                "Login gagal! Email atau password salah.",
-                              ),
-                              backgroundColor: Colors.red,
-                              duration: Duration(seconds: 2),
-                            ),
-                          );
-                        }
-                      },
-                      child: const Text("Login"),
+                    // Tombol login
+                    CustomButton(
+                      color: Colors.blueAccent,
+                      press: _login,
+                      textt: "Login",
+                      textColor: Colors.white,
                     ),
-
-                    SizedBox(height: 10),
-
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const Register(),
-                          ),
-                        );
-                      },
-                      child: const Text("Register"),
+                    const SizedBox(height: 10),
+                    const Text("atau"),
+                    const SizedBox(height: 10),
+                    // Tombol register
+                    CustomButton(
+                      color: Colors.blueAccent,
+                      press: _register,
+                      textt: "Register",
+                      textColor: Colors.white,
                     ),
                   ],
                 ),
